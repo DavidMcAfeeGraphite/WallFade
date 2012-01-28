afbbc70b628516658f1a45f29dc16674c4666b58
David McAfee <David@Davids-MacBook-Air.local>
2012-01-28 18:41:26 +1030

    Initial Commit

---
+++ WallFade/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WallFade
+//
+//  Created by David McAfee on 28/01/12.
+//  Copyright (c) 2012 Graphite iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ WallFade/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WallFade
+//
+//  Created by David McAfee on 28/01/12.
+//  Copyright (c) 2012 Graphite iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
